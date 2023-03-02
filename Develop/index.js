6a7546b12273096aec13d47a4978c551d9ce716b
jake rudlong <jrudlong@gmail.com>
2023-03-02 15:23:22 -0600

    adds starter code

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,27 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        message:'what is your name?',
+        name:'name'
+    },
+    {
+        type:'input',
+        message:'what is your project called?',
+        name:'title'
+    }
+    {
+        type:'input',
+        message:'what is your project called?',
+        name:'title'
+    }
 
+];
+//
+inquirer.prompt([...questions]).then
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
